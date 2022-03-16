5d4481273ebfae81504196a47a2eea66dd606348
Bomin <iambxmk@gmail.com>
2022-03-16 21:49:01 +0900

    styled jsx: 각 컴포넌트로 범위가 한정되어 있어 다른 컴포넌트에 영향을 끼치지 않음

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,14 +1,23 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from './NavBar.module.css'; // css 모듈 패턴
 
 export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
-      {/* css 여러가지 쓸 때 백틱으로 문자열처럼 만들어주거나 배열로 만들 수 있음 */}
-      <Link href="/"><a className={`${styles.link} ${router.pathname === "/" ? styles.active : 'gray'}`}>HOME</a></Link>
-      <Link href="/about"><a className={[styles.link,router.pathname === "/about" ? styles.active : 'gray'].join(" ")}>ABOUT</a></Link>
+      <Link href="/"><a className="active">HOME</a></Link>
+      <Link href="/about"><a>ABOUT</a></Link>
+      <style jsx>{`
+        nav { 
+          background-color: pink;
+        }
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: ${props.color}
+        }
+      `}</style>
     </nav>
   )
 }
