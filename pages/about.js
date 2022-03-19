5ce678b518591bb12c535a123b4f81c0ed65349c
Bomin <iambxmk@gmail.com>
2022-03-19 21:41:36 +0900

    meta, 작성자 정보 등을 Head를 따로 뺀 파일에 같이 작성

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,11 @@
 import Head from "next/head";
 import NavBar from "../components/NavBar";
+import Seo from "../components/Seo";
 
 export default function AboutPage() {
   return (
     <div>
-      <Head>
-        <title>About | Next Movies</title>
-      </Head>
+      <Seo title="About" />
       <h1>about</h1>
     </div>
   );
