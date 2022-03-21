87322a196e529f20ffd92df347b396a95a1bcbb5
Bomin <iambxmk@gmail.com>
2022-03-21 21:24:15 +0900

    next.config.js에서 redirects, rewrites 작성

--- next.config.js
+++ next.config.js
@@ -1,6 +1,34 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = process.env.API_KEY;
+
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    // /contact 주소를 치면 네이버로 리다이렉트 됨
+    return [
+      {
+        source: "/contact",
+        destination: 'https://www.nave.com',
+        permanent: false,
+      },
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-sexy-blog/:path*",
+        permanent: false,
+      },
+    ]
+  },
+  // rewrite는 리다이렉트 하긴 하지만 url을 바꾸지 않음
+  async rewrites() {
+    return [
+      {
+        source: '/api/movies',
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
