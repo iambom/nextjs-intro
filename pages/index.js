3973cd30987be2fc941a963db2396ec092e1e450
Bomin <iambxmk@gmail.com>
2022-03-26 22:39:23 +0900

    getServerSideProps() 함수는 서버에서만 작동하게 됨

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,11 @@ import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
 
-export default function Home() {
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    (async () => {
-      const { results } = await (
-        await fetch('/api/movies')
-      ).json();
-      setMovies(results)
-    })();
-  }, [])
+export default function Home({ results }) {
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map(movie => (
+      {results?.map(movie => (
        <div className="movie" key={movie.id}>
         <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
         <h4>{movie.original_title}</h4>
@@ -47,3 +37,23 @@ export default function Home() {
     
   );
 }
+
+export async function getServerSideProps() {
+// 어떤 코드던 이 안에서 쓰면 서버에서만 작동함
+// API key 등을 여기서 써주면 client에서 볼 수 없음
+  const { results } = await (await fetch(`https://localhost:3000/api/movies`)).json();
+  // object를 리턴할 수 있고 props라는 키를 가질 수 있다. props 안에는 어떤 것이든 넣을 수 있다. 
+  return {
+    props: {
+      results,
+    }
+  }
+}
+
+/*
+
+1. getServerSideProps()는 서버에서 실행됨
+2. 함수 안에서 무엇을 리턴하던지, 이 return한 데이터를 props로써 page에게 줌
+3. 하지만 fetch를 이 함수 안에서 할 때, API load가 느려지면 사용자들은 아무것도 못 볼 수 있음 
+
+*/
